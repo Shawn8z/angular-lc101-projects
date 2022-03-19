1ceacb8b6b51da922dca3dc702b30fcdf18b6b7e
Shawn <lcorel86@gmail.com>
2022-03-19 17:22:01 -0500

    exercise done

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -10,7 +10,6 @@ export class FavPhotosComponent implements OnInit {
   image1 = 'https://cdn.onebauer.media/one/empire-tmdb/films/157336/images/xu9zaAevzQ5nnrsXN6JcahLnG4i.jpg?format=jpg&quality=80&width=960&height=540&ratio=16-9&resize=aspectfill';
   image2 = 'https://fatimaaltaf.files.wordpress.com/2017/09/interstellar_7.jpg';
   image3 = 'https://cdn.europosters.eu/image/750/posters/interstellar-go-further-i23316.jpg';
-
   constructor() { }
 
   ngOnInit() {
